24461b695845cea1d46b58b33cf2ae91267d8a0c
Pooja Patil <poojapatil010677@gmail.com>
2020-06-06 12:52:03 +0530

    invert a binary tree

---
+++ invertBT.c
@@ -0,0 +1,23 @@
+// Invert a binary tree
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     struct TreeNode *left;
+ *     struct TreeNode *right;
+ * };
+ */
+
+
+struct TreeNode* invertTree(struct TreeNode* root){
+    if(root==NULL) return;
+    else
+    {
+        invertTree(root->left);
+        invertTree(root->right);
+        struct TreeNode* temp = root->left;
+        root->left = root->right;
+        root->right = temp;
+        return root;
+    }
+}
