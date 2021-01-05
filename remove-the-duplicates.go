5145aae81e681bce5b8e50c6f757c3d3a479be66
unknown <poojapatil010677@gmail.com>
2021-01-05 16:08:25 +0530

    05/01/2020

---
+++ remove-the-duplicates.go
@@ -0,0 +1,21 @@
+/**
+ * Definition for singly-linked list.
+ * type ListNode struct {
+ *     Val int
+ *     Next *ListNode
+ * }
+ */
+func deleteDuplicates(head *ListNode) *ListNode {
+
+    cur := head
+    for cur != nil {
+        if cur.Next!=nil && cur.Val == cur.Next.Val {
+            cur.Next = cur.Next.Next
+            continue
+        }
+        
+        cur = cur.Next
+    }
+    return head
+}
+
