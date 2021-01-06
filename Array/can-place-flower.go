9abd74b1da7af8f9d5d5ffe5793f0acd24af4c37
unknown <poojapatil010677@gmail.com>
2021-01-06 13:15:24 +0530

    06/01/2021

---
+++ Array/can-place-flower.go
@@ -0,0 +1,29 @@
+/*
+You have a long flowerbed in which some of the plots are planted, and some are not. However, flowers cannot be planted in adjacent plots.
+
+Given an integer array flowerbed containing 0's and 1's, where 0 means empty and 1 means not empty, and an integer n, return if n new flowers can be planted in the flowerbed without violating the no-adjacent-flowers rule.
+
+ 
+
+Example 1:
+
+Input: flowerbed = [1,0,0,0,1], n = 1
+Output: true
+Example 2:
+
+Input: flowerbed = [1,0,0,0,1], n = 2
+Output: false
+*/
+
+func canPlaceFlowers(flowerbed []int, n int) bool {
+    i := 0
+    count := 0
+    for i < len(flowerbed) {
+        if flowerbed[i] == 0 && (i == 0 || flowerbed[i-1] == 0) && (i == len(flowerbed)-1 || flowerbed[i+1] == 0) {
+            flowerbed[i] = 1
+            count++
+        }
+        i++
+    }
+    return count >= n
+}
